89928cd19bc865cde7b26cd263d2fc00e04ab785
FreeGameDev <FreeGameDev@gmail.com>
2018-05-24 14:48:19 -0700

    Added Perlin Noise To TerrainOBject

---
+++ Assets/fovTerrainGenerator/Scripts/_Classes/PerlinNoise.cs
@@ -0,0 +1,29 @@
+﻿//Reference: https://youtu.be/dycHQFEz8VI?t=271
+using UnityEngine;
+
+public class PerlinNoise: MonoBehaviour {
+
+    public int heightScale = 10;
+    public float DetailSize = 0.9f;
+
+    void Start () {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int v = 0; v < vertices.Length; v++)
+        {
+            float x = Mathf.Round(vertices[v].x);
+            float z = Mathf.Round(vertices[v].z);
+
+            vertices[v].y = Mathf.PerlinNoise(
+               ((x * this.transform.localScale.x) + this.transform.position.x) * DetailSize,
+               ((z * this.transform.localScale.z) + this.transform.position.z) * DetailSize) * heightScale;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+
+    }
+    
+
+}
